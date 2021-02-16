2232ed35d1c945eb36c3685c896eecf353eb308b
sowmyatellapragada <sowmyatellapragada01@gmail.com>
2021-02-16 20:29:21 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Asynchronous.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asynchronous.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        //public async Task<ViewResult> Index()
+        //{
+        //    //long? length = await AsyncMethods.GetPageLength();
+        //    //return View(new string[] { $"Length: {length}"
+        //    //});
+        //    //List<string> output = new List<string>();
+        //    //await foreach (long? len in AsyncMethods.GetPageLengths(output,"google.com", "microsoft.com","amazon.com"))
+        //    //{
+        //    //    output.Add($"Page length: { len}");
+        //    //}
+        //    //return View(output);
+           
+        //}
+        public ViewResult Index()
+        {
+            var products = new[] {new { Name = "Kayak", Price = 275M },new { Name = "Lifejacket", Price = 48.95M },new { Name = "Soccer ball", Price = 19.50M },new { Name = "Corner flag", Price = 34.95M }};
+            //return View(products.Select(p => $"Name{ p.Price} "));
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name},{ nameof(p.Price)}: { p.Price}"));
+        }
+    }
+}
---
+++ Models/AsyncMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace Asynchronous.Models
+{
+    public class AsyncMethods
+    {
+
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await
+            client.GetAsync("http://google.com");
+            return
+            httpMessage.Content.Headers.ContentLength;
+        }
+        public static async IAsyncEnumerable<long?>GetPageLengths(List<string> output, params string[] urls)
+        {
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await
+                client.GetAsync($"http://{url}");
+                output.Add($"Completed request for {url}");
+                yield return
+                httpMessage.Content.Headers.ContentLength;
+            }
+
+        }
+    }
+}
